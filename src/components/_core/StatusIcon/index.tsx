1726b9fd198fcec332a8f460b5f5e2a6d55bcefe
Max Miller <mpm1900@users.noreply.github.com>
2021-08-14 12:33:21 -0500

    more icons

--- src/components/_core/StatusIcon/index.tsx
+++ src/components/_core/StatusIcon/index.tsx
@@ -2,15 +2,42 @@ import { FunctionComponent } from 'react'
 import { Status } from '../../../types/status/status'
 import { BoxProps } from '../Box'
 import { Icon } from '../Icon'
-import { ReactComponent as Dazed } from '../../../icons/lorc/star-swirl.svg'
-import { ReactComponent as Shocked } from '../../../icons/lorc/lightning-trio.svg'
 import { ReactComponent as Burned } from '../../../icons/carl-olsen/flame.svg'
+import { ReactComponent as Cursed } from '../../../icons/lorc/cursed-star.svg'
+import { ReactComponent as Dazed } from '../../../icons/lorc/star-swirl.svg'
+import { ReactComponent as Evasive } from '../../../icons/lorc/sprint.svg'
+import { ReactComponent as Focused } from '../../../icons/delapouite/polar-star.svg'
+import { ReactComponent as Protected } from '../../../icons/delapouite/vibrating-shield.svg'
+import { ReactComponent as Resting } from '../../../icons/delapouite/night-sleep.svg'
+import { ReactComponent as Shocked } from '../../../icons/lorc/lightning-trio.svg'
+import { ReactComponent as Warped } from '../../../icons/delapouite/extra-time.svg'
+
+import { ReactComponent as Normal } from '../../../icons/delapouite/plain-circle.svg'
+import { ReactComponent as Fire } from '../../../icons/sbed/flamer.svg'
+import { ReactComponent as Water } from '../../../icons/lorc/drop.svg'
+import { ReactComponent as Thunder } from '../../../icons/lorc/power-lightning.svg'
+import { ReactComponent as Grass } from '../../../icons/delapouite/oak-leaf.svg'
+import { ReactComponent as Earth } from '../../../icons/delapouite/stone-pile.svg'
+import { ReactComponent as Air } from '../../../icons/lorc/fluffy-wing.svg'
+import { ReactComponent as Ghost } from '../../../icons/lorc/spectre.svg'
+import { ReactComponent as Dark } from '../../../icons/sbed/death-skull.svg'
+import { ReactComponent as Light } from '../../../icons/sbed/pulse.svg'
 import { ReactComponent as Na } from '../../../icons/delapouite/perspective-dice-six-faces-random.svg'
 
 const iconMap: Record<string, FunctionComponent> = {
+  Cursed: Cursed,
   Burned: Burned,
   Dazed: Dazed,
+  Evasive: Evasive,
+  Focused: Focused,
+  Protected: Protected,
+  Resting: Resting,
   Shocked: Shocked,
+  Warped: Warped,
+
+  // TODO: find unique icon
+  Wet: Water,
+  'Earth Immunity': Earth,
 }
 
 export type StatusIconProps = BoxProps & {
