263ef986a893ea367ff8905d429649d821050d0d
Max Miller <mpm1900@users.noreply.github.com>
2021-08-17 10:14:30 -0500

    polish

--- src/domain/Combat/index.tsx
+++ src/domain/Combat/index.tsx
@@ -9,7 +9,12 @@ export const Combat = () => {
   return initialized ? (
     <Box flex={1} overflow='hidden'>
       <CombatHeader />
-      <Box flex='1' flexDirection='row' overflow='auto'>
+      <Box
+        flex='1'
+        flexDirection='row'
+        overflow='auto'
+        maxHeight='calc(100vh - 127px)'
+      >
         <CombatParty index={0} side='left' />
         <CombatBody />
         <CombatParty index={1} side='right' />
